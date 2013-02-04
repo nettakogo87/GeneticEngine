f6b03c4582317558f58cf49a8a0ab2c7f4fe6bd8
Vladimir <bratstakan@gmail.com>
2013-02-04 17:49:40 +0300

    Простая особь

---
+++ GeneticEngine/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeneticEngine
+{
+    /**
+     * Особь должна реализовывать интрефейс IPerson, и хранить массив генов(генотип).
+     */
+    public class Person : IPerson
+    {
+        public int[] Genotype { get; set; }
+        public int TypeOfSelection { get; set; }
+        public int TypeOfMutation { get; set; }
+        public int TypeOfCrossingover { get; set; }
+    }
+}
