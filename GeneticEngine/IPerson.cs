71851fa08a212130824db6ea6a8ca2b0c2b59dda
Vladimir <bratstakan@gmail.com>
2013-02-04 17:49:23 +0300

    Интерфейс особи

---
+++ GeneticEngine/IPerson.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeneticEngine
+{
+    /**
+     * Для контроля за тем как получаетются особи, каждая особь хранит в себе данные об этом:
+     * "Тип селекции, тип мутации, тип скрещивания".
+     */
+    public interface IPerson
+    {
+        int TypeOfSelection { get; set; }
+        int TypeOfMutation { get; set; }
+        int TypeOfCrossingover { get; set; }
+    }
+}
