50d7959a8f1b26f0955dd98946d54fea657365b6
Vladimir <bratstakan@gmail.com>
2013-02-14 00:16:00 +0300

    Собственно абстрактный класс для Track

---
+++ GeneticEngine/Track/AbstractTrack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GeneticEngine.Graph;
+
+namespace GeneticEngine.Track
+{
+    public abstract class AbstractTrack
+    {
+        public const int AggregateOfGenotype = -1;
+        public abstract double GetTrackLength(IGraph graph);
+
+        protected AbstractTrack(int[] trackPoints)
+        {
+            Genotype = new int[trackPoints.Length];
+            trackPoints.CopyTo(Genotype, 0);
+        }
+
+        public AbstractTrack(int countOfAllele, bool autofill)
+        {
+            Genotype = new int[countOfAllele];
+            for (int i = 0; i < countOfAllele; i++)
+            {
+                Genotype[i] = AggregateOfGenotype;
+            }
+            TypeOfCrossingover = "Not";
+            TypeOfMutation = "Not";
+            TypeOfSelection = "Not";
+
+            if (autofill)
+            {
+                Random randomChromosome = new Random();
+                int newRandomChromosome = randomChromosome.Next(countOfAllele - 1);
+                for (int i = 0; i < countOfAllele; i++)
+                {
+                    while (Genotype.Contains(newRandomChromosome))
+                    {
+                        newRandomChromosome = randomChromosome.Next(countOfAllele);
+                    }
+                    Genotype[i] = newRandomChromosome;
+                }
+            }
+        }
+
+        public abstract AbstractTrack EmptyClone();
+
+        public int[] Genotype { get; set; }
+        public string TypeOfSelection { get; set; }
+        public string TypeOfMutation { get; set; }
+        public string TypeOfCrossingover { get; set; }
+    }
+}
