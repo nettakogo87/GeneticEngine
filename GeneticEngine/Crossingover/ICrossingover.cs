41f99e7c4b00c8888911c6443d8cc7cd19ae6ae7
Vladimir <bratstakan@gmail.com>
2013-02-13 16:02:26 +0300

    Скрещивание 1-точечное и интерфейс скрещивания

---
+++ GeneticEngine/Crossingover/ICrossingover.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GeneticEngine.Track;
+
+namespace GeneticEngine.Crossingover
+{
+    public interface ICrossingover
+    {
+        void Crossingover(ITrack firstParent, ITrack secondParent, ITrack firstChild, ITrack secondChaild);
+        string GetName();
+    }
+}
