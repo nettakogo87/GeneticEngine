1ef31d851b04ee18e28783b00d5e49505a677a97
Vladimir <bratstakan@gmail.com>
2013-02-15 00:14:08 +0300

    Добавил новый способ скрещивания - инверсию. И тутже затестировал!

---
+++ GeneticEngine/Crossingover/InversionCrossingover.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GeneticEngine.Track;
+
+namespace GeneticEngine.Crossingover
+{
+    public class InversionCrossingover : ICrossingover
+    {
+        public void Crossingover(AbstractTrack firstParent, AbstractTrack secondParent, AbstractTrack firstChild,
+                                 AbstractTrack secondChaild)
+        {
+            int countOfAlleles = firstParent.Genotype.Length;
+            for (int i = 0; i < countOfAlleles; i++)
+            {
+                firstChild.Genotype[i] = firstParent.Genotype[i];
+                secondChaild.Genotype[i] = secondParent.Genotype[i];
+            }
+
+            Random randomPositionOne = new Random();
+            Random randomPositionTwo = new Random();
+            int positionOne = randomPositionOne.Next(1, countOfAlleles - 1);
+            int positionTwo = randomPositionTwo.Next(1, countOfAlleles - 1);
+            while (positionOne == positionTwo)
+            {
+                positionTwo = randomPositionTwo.Next(1, countOfAlleles);
+            }
+            if (positionOne > positionTwo)
+            {
+                int buff = positionOne;
+                positionOne = positionTwo;
+                positionTwo = buff;
+            }
+            double delimetr = Math.Ceiling((Convert.ToDouble(positionOne + positionTwo)/2.0));
+            for (int i = positionOne, j = positionTwo; i < delimetr; i++, j--)
+            {
+                int buff = firstChild.Genotype[i];
+                firstChild.Genotype[i] = firstChild.Genotype[j];
+                firstChild.Genotype[j] = buff;
+                buff = secondChaild.Genotype[i];
+                secondChaild.Genotype[i] = secondChaild.Genotype[j];
+                secondChaild.Genotype[j] = buff;
+            }
+        }
+
+        public string GetName()
+        {
+            return "InversionCrossingover";
+        }
+    }
+}
---
+++ GeneticEngineTests/Crossingover/InversionCrossingoverTest.cs
@@ -0,0 +1,58 @@
+﻿using GeneticEngine.Crossingover;
+using GeneticEngine.Graph;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using GeneticEngine.Track;
+
+namespace GeneticEngineTests.Crossingover
+{
+    /// <summary>
+    ///Это класс теста для InversionCrossingoverTest, в котором должны
+    ///находиться все модульные тесты InversionCrossingoverTest
+    ///</summary>
+    [TestClass()]
+    public class InversionCrossingoverTest
+    {
+        private const int CountOfAllele = 4;
+        private AbstractTrack _parent1;
+        private AbstractTrack _parent2;
+        private AbstractTrack _child1;
+        private AbstractTrack _child2;
+        private IGraph _graph;
+
+        [TestInitialize()]
+        public void MyTestInitialize()
+        {
+            int[] trackPoints1 = new int[] { 0, 2, 1, 3 };
+            int[] trackPoints2 = new int[] { 1, 3, 0, 2 };
+            _parent1 = new UnclosedTrack(trackPoints1);
+            _parent2 = new UnclosedTrack(trackPoints2);
+            _child1 = new UnclosedTrack(CountOfAllele, false);
+            _child2 = new UnclosedTrack(CountOfAllele, false);
+            int[,] ribs = new int[6, 2] { { 0, 1 }, { 0, 2 }, { 0, 3 }, { 1, 2 }, { 1, 3 }, { 2, 3 } };
+            double[] weights = new double[] { 3, 5, 10, 7, 8, 9 };
+            _graph = new UndirectedConnectedGraph(ribs, weights);
+        }
+
+        /// <summary>
+        ///Тест для Crossingover
+        ///</summary>
+        [TestMethod()]
+        public void CrossingoverTest()
+        {
+            InversionCrossingover target = new InversionCrossingover();
+            target.Crossingover(_parent1, _parent2, _child1, _child2);
+            Assert.IsTrue(!Array.Exists(_child1.Genotype, NegativeNumber));
+            Assert.IsTrue(!Array.Exists(_child2.Genotype, NegativeNumber));
+        }
+
+        private static bool NegativeNumber(int i)
+        {
+            if (i < 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
