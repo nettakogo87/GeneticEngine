291b6fcd94b7b90ff76b2525b5703826e3aa266a
Vladimir <bratstakan@gmail.com>
2013-02-20 00:10:22 +0300

    [RouletteSellection] Добавлен класс для селекции с помощью рулетки, почти затестирован

---
+++ GeneticEngine/Selection/RouletteSelection.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GeneticEngine;
+using GeneticEngine.Graph;
+using GeneticEngine.Track;
+
+namespace GeneticEngine.Selection
+{
+    public class RouletteSelection : ISelection
+    {
+        private string _name;
+
+        public RouletteSelection()
+        {
+            _name = "RouletteSelection";
+        }
+
+        public void Selection(AbstractTrack[] parentTracks, AbstractTrack[] childTracks, IGraph graph)
+        {
+            int countOfTracks = parentTracks.Length;
+            AbstractTrack[] allTracks = new AbstractTrack[countOfTracks * 2];
+            for (int i = 0; i < countOfTracks; i++)
+            {
+                allTracks[i] = parentTracks[i].Clone();
+                allTracks[i + countOfTracks] = childTracks[i].Clone();
+            }
+
+            Random random = new Random();
+            double sumOfFitness = 0;
+            for (int i = 0; i < countOfTracks * 2; i++)
+            {
+                sumOfFitness += allTracks[i].GetTrackLength(graph);
+            }
+            double[] sectorsOfWheel = new double[countOfTracks * 2];
+            for (int i = 0; i < countOfTracks * 2; i++)
+            {
+                sectorsOfWheel[i] = allTracks[i].GetTrackLength(graph) / sumOfFitness * 100; // формула для поиска размера сектров колеса рулетки
+            }
+            for (int i = 0; i < countOfTracks; i++)
+            {
+                int fallenSector = random.Next(100);
+                double sum = 0;
+                for (int j = 0; j < sectorsOfWheel.Length; j++)
+                {
+                    sum += sectorsOfWheel[j];
+                    if (sum > fallenSector)
+                    {
+                        parentTracks[i] = allTracks[j];
+                        break;
+                    }
+                }
+            }
+        }
+
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+    }
+}
---
+++ GeneticEngineTests/Selection/RouletteSelectionTest.cs
@@ -0,0 +1,51 @@
+﻿using GeneticEngine.Selection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using GeneticEngine.Track;
+using GeneticEngine.Graph;
+
+namespace GeneticEngineTests.Selection
+{
+    /// <summary>
+    ///Это класс теста для RouletteSelectionTest, в котором должны
+    ///находиться все модульные тесты RouletteSelectionTest
+    ///</summary>
+    [TestClass()]
+    public class RouletteSelectionTest
+    {
+        private const int CountOfAllele = 4;
+        private const int CountOfTracks = 3;
+        private AbstractTrack[] _parents;
+        private AbstractTrack[] _childs;
+        private IGraph _graph;
+
+        [TestInitialize()]
+        public void MyTestInitialize()
+        {
+            _parents = new AbstractTrack[CountOfTracks];
+            _parents[0] = new UnclosedTrack(new int[] { 0, 1, 3, 2 });
+            _parents[1] = new UnclosedTrack(new int[] { 1, 0, 2, 3 });
+            _parents[2] = new UnclosedTrack(new int[] { 2, 0, 3, 1 });
+
+            _childs = new AbstractTrack[CountOfTracks];
+            _childs[0] = new UnclosedTrack(new int[] { 2, 1, 0, 3 });
+            _childs[1] = new UnclosedTrack(new int[] { 0, 3, 2, 1 });
+            _childs[2] = new UnclosedTrack(new int[] { 0, 1, 2, 3 });
+
+            int[,] ribs = new int[6, 2] { { 0, 1 }, { 0, 2 }, { 0, 3 }, { 1, 2 }, { 1, 3 }, { 2, 3 } };
+            double[] weights = new double[] { 3, 5, 10, 7, 8, 9 };
+            _graph = new UndirectedConnectedGraph(ribs, weights);
+        }
+
+        /// <summary>
+        ///Тест для Selection
+        ///</summary>
+        [TestMethod()]
+        public void SelectionTest()
+        {
+            RouletteSelection target = new RouletteSelection();
+            target.Selection(_parents, _childs, _graph);
+            
+        }
+    }
+}
