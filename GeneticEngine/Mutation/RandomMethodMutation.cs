27eed96dec700db8f52a9219e21890ad66464f05
Vladimir <bratstakan@gmail.com>
2013-02-24 19:51:36 +0300

    [Mutation] Добавлен класс для выбора случайного метода мутации

---
+++ GeneticEngine/Mutation/RandomMethodMutation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GeneticEngine.Track;
+
+namespace GeneticEngine.Mutation
+{
+    public class RandomMethodMutation : IMutation
+    {
+        public const string MutationName = "RandomMethodMutation";
+        private List<IMutation> _mutationList;
+
+        public RandomMethodMutation(List<IMutation> mutationList)
+        {
+            _mutationList = new List<IMutation>();
+            _mutationList = mutationList;
+        }
+
+        public void Mutation(AbstractTrack mutant)
+        {
+            Random random = new Random();
+            int numberOfMutation = random.Next(_mutationList.Count());
+            _mutationList[numberOfMutation].Mutation(mutant);
+        }
+
+        public string GetName()
+        {
+            return MutationName;
+        }
+    }
+}
