5347c3027de5441861ed385c98409dc1f1afba6b
Vladimir <bratstakan@gmail.com>
2013-02-24 19:34:31 +0300

    [Selection] Добавлен класс абстрактной селекции, на всякий случай

---
+++ GeneticEngine/Selection/AbstractSelection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeneticEngine.Selection
+{
+    public abstract class AbstractSelection
+    {
+        public const string WithoutSelection = "WithoutSelection";
+    }
+}
