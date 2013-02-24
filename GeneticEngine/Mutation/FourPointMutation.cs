0e2da05aa2664077776f7b4f62938a9e4c78810d
Vladimir <bratstakan@gmail.com>
2013-02-24 17:33:24 +0300

    [Mutation] Добавлена и затестирована "Мутация четырехточечная"

---
+++ GeneticEngine/Mutation/FourPointMutation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GeneticEngine.Track;
+
+namespace GeneticEngine.Mutation
+{
+    public class FourPointMutation : IMutation
+    {
+        public void Mutation(AbstractTrack mutant)
+        {
+            int countOfAlleles = mutant.Genotype.Length;
+            Random randomPosition = new Random();
+            int firstPosition = randomPosition.Next(countOfAlleles);
+            int secondPosition = randomPosition.Next(countOfAlleles);
+            while (firstPosition == secondPosition)
+            {
+                secondPosition = randomPosition.Next(countOfAlleles);
+            }
+            int chromosome = mutant.Genotype[secondPosition];
+            mutant.Genotype[secondPosition] = mutant.Genotype[firstPosition];
+            mutant.Genotype[firstPosition] = chromosome;
+            int thirdPosition = randomPosition.Next(countOfAlleles);
+            while (thirdPosition == firstPosition || thirdPosition == secondPosition)
+            {
+                thirdPosition = randomPosition.Next(countOfAlleles);
+            }
+            int fourthPosition = randomPosition.Next(countOfAlleles);
+            while (fourthPosition == firstPosition || fourthPosition == secondPosition || fourthPosition == thirdPosition)
+            {
+                fourthPosition = randomPosition.Next(countOfAlleles);
+            }
+            chromosome = mutant.Genotype[thirdPosition];
+            mutant.Genotype[thirdPosition] = mutant.Genotype[fourthPosition];
+            mutant.Genotype[fourthPosition] = chromosome;
+        }
+
+        public string GetName()
+        {
+            return "FourPointMutation";
+        }
+    }
+}
---
+++ GeneticEngineTests/Mutation/FourPointMutationTest.cs
@@ -0,0 +1,44 @@
+﻿using GeneticEngine.Graph;
+using GeneticEngine.Mutation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using GeneticEngine.Track;
+
+namespace GeneticEngineTests.Mutation 
+{
+    /// <summary>
+    ///Это класс теста для FourPointMutationTest, в котором должны
+    ///находиться все модульные тесты FourPointMutationTest
+    ///</summary>
+    [TestClass()]
+    public class FourPointMutationTest : SupportingGeneticEngineTest
+    {
+        private const int CountOfAllele = 4;
+        private AbstractTrack _mutant;
+        private IGraph _graph;
+        private AbstractTrack _subject;
+
+        [TestInitialize()]
+        public void MyTestInitialize()
+        {
+            int[,] ribs = new int[6, 2] { { 0, 1 }, { 0, 2 }, { 0, 3 }, { 1, 2 }, { 1, 3 }, { 2, 3 } };
+            double[] weights = new double[] { 3, 5, 10, 7, 8, 9 };
+            _graph = new UndirectedConnectedGraph(ribs, weights);
+            int[] trackPoints = new int[] { 0, 2, 1, 3 };
+            _mutant = new UnclosedTrack(trackPoints, _graph);
+            _subject = new UnclosedTrack(trackPoints, _graph);
+        }
+
+
+        /// <summary>
+        ///Тест для Mutation
+        ///</summary>
+        [TestMethod()]
+        public void MutationTest()
+        {
+            FourPointMutation target = new FourPointMutation();
+            target.Mutation(_mutant);
+            Assert.IsFalse(TwoIntArrayEquals(_mutant.Genotype, _subject.Genotype));
+        }
+    }
+}
