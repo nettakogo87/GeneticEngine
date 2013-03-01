ae1f7ef20c867f26ed28619add528049e5d17260
Vladimir <bratstakan@gmail.com>
2013-03-01 15:49:51 +0300

    [GEngine][Tests] Добавлены тесты для проверки случайно выбирающихся алгоритмов селекции, кроссинговера и мутации.

--- GeneticEngineTests/GeneticEngineTest.cs
+++ GeneticEngineTests/GeneticEngineTest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using GeneticEngine.FitnessFunction;
 using GeneticEngine.Mutation;
@@ -27,9 +28,6 @@ namespace GeneticEngineTests
         [TestInitialize()]
         public void MyTestInitialize()
         {
-            _mutation = new TwoPointMutation();
-            _selection = new RankingSelection();
-            _crossingover = new TwoPointCrossingover();
             int[,] ribs = new int[6, 2] { { 0, 1 }, { 0, 2 }, { 0, 3 }, { 1, 2 }, { 1, 3 }, { 2, 3 } };
             double[] weights = new double[] { 3, 5, 10, 7, 8, 9 };
             _graph = new UndirectedConnectedGraph(ribs, weights);
@@ -44,14 +42,17 @@ namespace GeneticEngineTests
             _closedTracks[3] = new ClosedTrack(trackPoints4, _graph);
         }
 
-
         /// <summary>
         ///Тест для Run и фитнесфункции заключающейся в достижении заданного количества покалений.
         ///</summary>
         [TestMethod()]
         public void RunCounterTest()
         {
-            int expectCountOfGeneration = 120;
+            _mutation = new NotRandomMutation();
+            _selection = new TournamentSelection();
+            _crossingover = new CyclicalCrossingover();
+
+            int expectCountOfGeneration = 12000;
             IFitnessFunction fitnessFunction = new GenerationCounter(expectCountOfGeneration);
             int pCrossingover = 80; 
             int pMutation = 60; 
@@ -69,9 +70,50 @@ namespace GeneticEngineTests
         [TestMethod()]
         public void RunBestRipsTest()
         {
-            int bestReps = 12;
+            _mutation = new NotRandomMutation();
+            _selection = new TournamentSelection();
+            _crossingover = new CyclicalCrossingover();
+
+            int bestReps = 150;
             IFitnessFunction fitnessFunction = new BestReps(bestReps);
             int pCrossingover = 80; 
+            int pMutation = 60;
+            Array.Sort(_closedTracks);
+            double preBestResult = _closedTracks[0].GetTrackLength();
+            GEngine target = new GEngine(_closedTracks, pCrossingover, pMutation, fitnessFunction, _mutation, _crossingover, _selection);
+            target.Run();
+            Assert.AreEqual(bestReps, target.FitnessFunction.ActualCountOfReps);
+            Assert.IsTrue(preBestResult >= target.FitnessFunction.BestResult);
+        }
+
+        /// <summary>
+        ///Тест для Run и фитнесфункции заключающейся в достижении заданного количества повоторений лучшего результата.
+        ///</summary>
+        [TestMethod()]
+        public void RunRundomsMethodsTest()
+        {
+            List<IMutation> mutations = new List<IMutation>();
+            mutations.Add(new NotRandomMutation());
+            mutations.Add(new FourPointMutation());
+            mutations.Add(new TwoPointMutation());
+            _mutation = new RandomMethodMutation(mutations);
+
+            List<ISelection> selectios = new List<ISelection>();
+            selectios.Add(new TournamentSelection());
+            selectios.Add(new RouletteSelection());
+            selectios.Add(new RankingSelection());
+            _selection = new RandomMethodSelection(selectios);
+
+            List<ICrossingover> crossingovers = new List<ICrossingover>();
+            crossingovers.Add(new CyclicalCrossingover());
+            crossingovers.Add(new InversionCrossingover());
+            crossingovers.Add(new OnePointCrossingover());
+            crossingovers.Add(new TwoPointCrossingover());
+            _crossingover = new RandomMethodCrossingover(crossingovers);
+
+            int bestReps = 150;
+            IFitnessFunction fitnessFunction = new BestReps(bestReps);
+            int pCrossingover = 80;
             int pMutation = 60;
             Array.Sort(_closedTracks);
             double preBestResult = _closedTracks[0].GetTrackLength();
