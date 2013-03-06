0dffc5207bb050cd81be39a5e0af93953de7f8d2
Vladimir <bratstakan@gmail.com>
2013-03-06 17:25:04 +0300

    [ProxyOperation][Selection] Добавлен класс для хранения дополнительных данных для алгоритмов селекции.

---
+++ GeneticEngine/ProxyOperation/ProxySelection.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GeneticEngine.Selection;
+
+namespace GeneticEngine.ProxyOperation
+{
+    public class ProxySelection
+    {
+        private ISelection _selection;
+        private int _numberOfGoodStarts;
+        private int _numberOfBadStarts;
+        private TimeSpan _operationTime;      // часы:минуты:секунды.милисекунды
+        public ProxySelection(ISelection selection)
+        {
+            _selection = selection;
+            _numberOfGoodStarts = 0;
+            _numberOfBadStarts = 0;
+        }
+
+        public ISelection GetSelection()
+        {
+            return _selection;
+        }
+        public int NumberOfStarts
+        {
+            get { return _numberOfGoodStarts + _numberOfBadStarts; }
+        }
+        public int NumberOfGoodStarts
+        {
+            get { return _numberOfGoodStarts; }
+        }
+        public void AddGoodStart()
+        {
+            _numberOfGoodStarts++;
+        }
+        public int NumberOfBadStarts
+        {
+            get { return _numberOfBadStarts; }
+        }
+        public void AddBadStart()
+        {
+            _numberOfBadStarts++;
+        }
+        public TimeSpan GetOperationTime()
+        {
+            return _operationTime;
+        }
+        public void AddOperationTime(TimeSpan time)
+        {
+            _operationTime += time;
+        }
+        public double GetProgress()
+        {
+            return Convert.ToDouble(_numberOfGoodStarts)/(Convert.ToDouble(NumberOfStarts) / 100);
+        }
+
+        public string SelectionName
+        {
+            get { return _selection.GetName(); }
+        }
+    }
+}
