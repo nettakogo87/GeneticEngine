0183911ca4056f471005fde8e489227c14ed1c17
Vladimir <bratstakan@gmail.com>
2013-03-06 15:24:17 +0300

    [ProxyOperation][Mutation] Добавлен класс для хронения дополнительных данных,  связанных с алгоритмами мутации.

---
+++ GeneticEngine/ProxyOperation/ProxyMutation.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GeneticEngine.Mutation;
+/**
+ * Класс для того что бы можно было мэпить на базу данных то, что в нем лежит
+ * 
+ */
+namespace GeneticEngine.ProxyOperation
+{
+    public class ProxyMutation
+    {
+        private IMutation _mutation;
+        private int _numberOfGoodStarts;
+        private int _numberOfBadStarts;
+        private TimeSpan _operationTime;      // часы:минуты:секунды.милисекунды
+        private double _progress;
+        public ProxyMutation(IMutation mutation)
+        {
+            _mutation = mutation;
+            _numberOfGoodStarts = 0;
+            _numberOfBadStarts = 0;
+            _progress = 0;
+        }
+
+        public IMutation GetMutation()
+        {
+            return _mutation;
+        }
+        public int NumberOfStarts
+        {
+            get { return _numberOfGoodStarts + _numberOfBadStarts; }
+        }
+        public int NumberOfGoodStarts
+        {
+            get { return _numberOfGoodStarts; }
+        }
+        public void AddGoodStart()
+        {
+            _numberOfGoodStarts++;
+        }
+        public int NumberOfBadStarts
+        {
+            get { return _numberOfBadStarts; }
+        }
+        public void AddBadStart()
+        {
+            _numberOfBadStarts++;
+        }
+        public TimeSpan GetOperationTime()
+        {
+            return _operationTime;
+        }
+        public void AddOperationTime(TimeSpan time)
+        {
+            _operationTime += time;
+        }
+        public double GetProgress()
+        {
+            return _progress;
+        }
+        public void IncreaseProgress(double procentGain)
+        {
+            _progress = (_progress + procentGain) / NumberOfStarts;
+        }
+        public string MutationName
+        {
+            get { return _mutation.GetName(); }
+        }
+    }
+}
