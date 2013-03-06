6bbbf616a992e2a5dc6b89d6e27411abf3591e75
Vladimir <bratstakan@gmail.com>
2013-03-06 17:26:19 +0300

    [Selection] Класс случайного выбора алгоритма селекции, заменен на класс сравнения алгоритмов селекции.

---
+++ GeneticEngine/Selection/QualityCountsSelection.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GeneticEngine.Graph;
+using GeneticEngine.ProxyOperation;
+using GeneticEngine.Track;
+
+namespace GeneticEngine.Selection
+{
+    public class QualityCountsSelection : AbstractSelection, ISelection
+    {
+        public const string SelectionName = "QualityCountsSelection";
+        private List<ProxySelection> _proxySelectionList;
+        private int _index; // mutation algorithm Index
+
+        public QualityCountsSelection(List<ProxySelection> proxySelectionList)
+        {
+            _proxySelectionList = new List<ProxySelection>();
+            _proxySelectionList = proxySelectionList;
+            _index = 0;
+        }
+
+        public void Selection(AbstractTrack[] parentTracks, AbstractTrack[] childTracks)
+        {
+            if (_index == _proxySelectionList.Count)
+            {
+                _index = 0;
+            }
+
+            int countOfTracks = parentTracks.Length;
+            AbstractTrack[] allTracks = new AbstractTrack[parentTracks.Length * 2];
+            for (int i = 0; i < countOfTracks; i++)
+            {
+                allTracks[i] = parentTracks[i].Clone();
+                allTracks[i + countOfTracks] = childTracks[i].Clone();
+            }
+            Array.Sort(allTracks);
+            double currentResult = allTracks[0].GetTrackLength();
+
+            DateTime startTime = DateTime.Now;
+            _proxySelectionList[_index].GetSelection().Selection(parentTracks, childTracks);
+            DateTime endTime = DateTime.Now;
+            TimeSpan time = endTime - startTime;
+            _proxySelectionList[_index].AddOperationTime(time);
+
+            AbstractTrack[] newParentTracks = new AbstractTrack[parentTracks.Length];
+            for (int i = 0; i < countOfTracks; i++)
+            {
+                newParentTracks[i] = parentTracks[i].Clone();
+            }
+            Array.Sort(newParentTracks);
+            newParentTracks[0].GetTrackLength();
+            double newResult = newParentTracks[0].GetTrackLength();
+            if (0 == newResult.CompareTo(currentResult))
+            {
+                _proxySelectionList[_index].AddGoodStart();
+            }
+            else
+            {
+                _proxySelectionList[_index].AddBadStart();
+            }
+            _index++;
+        }
+
+        public string GetName()
+        {
+            return SelectionName;
+        }
+    }
+}
