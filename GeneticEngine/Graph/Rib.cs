59a2bd0f4c22766f9e80026225b5b428fa20f5c0
Vladimir <bratstakan@gmail.com>
2013-03-19 21:05:38 +0300

    [Graph] Добавил структуру представляющую собой ребро. Ребро имеет 2 вершины и вес.

---
+++ GeneticEngine/Graph/Rib.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeneticEngine.Graph
+{
+    public struct Rib
+    {
+        public int StartNode { get; private set; }
+        public int EndNode { get; private set; }
+        public double Weight { get; private set; }
+
+        public Rib(int startNode, int endNode, double weight) : this()
+        {
+            StartNode = startNode;
+            EndNode = endNode;
+            Weight = weight;
+        }
+    }
+}
