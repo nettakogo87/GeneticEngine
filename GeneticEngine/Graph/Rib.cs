ff4325b2b51873b86e22967365a9ff3f701c686e
Vladimir <bratstakan@gmail.com>
2013-03-24 16:17:40 +0300

    [Graph] Класс ребра теперь сериализуется

--- GeneticEngine/Graph/Rib.cs
+++ GeneticEngine/Graph/Rib.cs
@@ -2,14 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml.Serialization;
 
 namespace GeneticEngine.Graph
 {
+    [Serializable]
     public struct Rib
     {
         public int StartNode { get; private set; }
         public int EndNode { get; private set; }
-        public double Weight { get; private set; }
+        public double Weight { get; set; }
 
         public Rib(int startNode, int endNode, double weight) : this()
         {
