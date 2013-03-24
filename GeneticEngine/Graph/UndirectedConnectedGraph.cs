52afc6dcc9fb9396cc6c01d0cf8e79d3d7dd2eaf
Vladimir <bratstakan@gmail.com>
2013-03-24 16:23:13 +0300

    [Graph] Добавил в граф еще один конструктор. Теперь можно создать ненаправленный граф передав ему число вешин.

--- GeneticEngine/Graph/UndirectedConnectedGraph.cs
+++ GeneticEngine/Graph/UndirectedConnectedGraph.cs
@@ -2,28 +2,48 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml.Serialization;
 
 namespace GeneticEngine.Graph
 {
-    [Serializable()]
+    [Serializable]
     public class UndirectedConnectedGraph : IGraph
     {
-        private List<Rib> _ribs;
+        private Rib[] _ribs;
         public int CountOfRibs { get; set; }
+        public int CountOfNode { get; set; }
 
-        public UndirectedConnectedGraph(int[,] ribs, double[] weights)
+        public UndirectedConnectedGraph(int defaultCountOfNode)
         {
-            CountOfRibs = weights.Length;
-            _ribs = new List<Rib>();
-            for (int i = 0; i < CountOfRibs; i++)
+            CountOfNode = defaultCountOfNode;
+            CountOfRibs = (defaultCountOfNode * defaultCountOfNode - defaultCountOfNode) / 2;
+            _ribs = new Rib[CountOfRibs];
+            int k = 0;
+            for (int i = 0; i < defaultCountOfNode; i++)
             {
-                _ribs.Add(new Rib(ribs[i, 0], ribs[i, 1], weights[i]));
+                for (int j = i + 1; j < defaultCountOfNode; j++)
+                {
+                    _ribs[k] = new Rib(i, j, 1);
+                    k++;
+                }
             }
         }
 
-        public UndirectedConnectedGraph(List<Rib> ribs)
+        public UndirectedConnectedGraph(int[,] ribs, double[] weights)
         {
-            CountOfRibs = ribs.Count;
+            CountOfNode = ribs[weights.Length - 1, 1];
+            CountOfRibs = weights.Length;
+            _ribs = new Rib[weights.Length];
+            for (int i = 0; i < CountOfRibs; i++)
+            {
+                _ribs[i] = new Rib(ribs[i, 0], ribs[i, 1], weights[i]);
+            }
+        }
+
+        public UndirectedConnectedGraph(Rib[] ribs)
+        {
+            CountOfNode = ribs[ribs.Length - 1].EndNode + 1;
+            CountOfRibs = ribs.Length;
             _ribs = ribs;
         }
 
@@ -44,6 +64,11 @@ namespace GeneticEngine.Graph
         public Rib GetRib(int index)
         {
             return _ribs[index];
+        }
+
+        public void SetRib(int index, Rib newRib)
+        {
+            _ribs[index] = newRib;
         }
     }
 }
